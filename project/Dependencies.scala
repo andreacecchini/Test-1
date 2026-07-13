469b0de3feda8b346960ad4622f78bc50d56fab5
Andrea Cecchini <63281186+andreacecchini@users.noreply.github.com>
2026-07-13 12:17:52 +0200

    Initial commit

---
+++ project/Dependencies.scala
@@ -0,0 +1,23 @@
+// format: off
+import sbt.*
+
+/** Contains the project dependencies.
+  */
+object Dependencies {
+  object Modules {
+    /*
+     * Scalatest
+     */
+    lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.20"
+    /*
+     * Mockito
+     */
+    lazy val mockito = "org.scalatestplus" %% "mockito-5-23" % "3.2.20.0"
+  }
+  import Modules.*
+
+  /** A sequence of testing dependencies.
+   */
+  lazy val testing: Seq[ModuleID] = Seq(scalaTest, mockito) map (_ % Test)
+}
+// format: on
---
+++ project/plugins.sbt
@@ -0,0 +1,17 @@
+/*
+ * Scalafmt
+ *
+ */
+addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.6.1")
+/*
+ * Wartremover
+ */
+addSbtPlugin("org.wartremover" % "sbt-wartremover" % "3.5.7")
+/*
+ * Sbt-assembly
+ */
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "2.3.1")
+/*
+ * Sbt-scoverage
+ */
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "2.4.4")
